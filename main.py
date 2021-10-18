9951eb33a5100301cc2725f7a656e39d4ca850d7
Filips Alpe <filips.alpe@gmail.com>
2021-10-18 19:27:10 +0100

    Init project

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import time
+
+
+def main():
+    while (True):
+        print("I wish I had something to calculate. See you in 10 seconds.")
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
